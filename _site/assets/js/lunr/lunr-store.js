d77134e85e701c5149003608c8170916e1a85964
isaac-reid <isaacreid05@gmail.com>
2022-08-06 18:16:32 +0100

    commit

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Entanglement barriers in dual-unitary quantum circuits",
-        "excerpt":"The unwieldy title belies surprisingly simple and deep behaviour, pertaining to how quantum entanglement propagates over time in a 1D system of interacting qubits. This review is intended to capture the essence of the results first reported in this PRB paper, but will forgo the gory mathematical details. The interested...","categories": [],
-        "tags": [],
-        "url": "/2022/06/19/dual_unitary.html",
-        "teaser": null
-      }]
